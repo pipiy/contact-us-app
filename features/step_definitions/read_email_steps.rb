6d9cce5de774bbc708fe78b02dc7b2740cd07e7f
Artem Slabodnik <artem.slabodnik@gmail.com>
2012-09-27 14:44:17 +0300

    Rename the scenarios and improve the steps

--- features/step_definitions/read_email_steps.rb
+++ features/step_definitions/read_email_steps.rb
@@ -1,23 +1,18 @@
 When /^users sent a message from the contact us page$/ do
   steps %Q{
-  	Given I am on the home page
-  	When I press the contact us button
-  	And I fill in name
-	And I fill in email
-	And I fill in description
-	And I press the send button
+    Given I am on the home page
+    When I press the contact us button
+    And I fill in name
+    And I fill in email
+    And I fill in description
+    And I press the send button
   }
 end
-
-When /^I click on contact us button$/ do
-  click_on('Contact Us')
-end
-
 
 Then /^the message should include the user details and a message$/ do
   email.body.should include('Jack')
   email.body.should include('example@gmail.com')
   email.body.should include('Test description for sent messages')
-  email.subject.should == "Contact us"
+  email.subject.should == "Contact Us"
   email.to.should include("artem.slabodnik@gmail.com")
 end
