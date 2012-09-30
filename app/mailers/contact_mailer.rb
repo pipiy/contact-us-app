bd69843a941fa4e2f04fdea5d34d7a0bc94cfacc
Artem Slabodnik <artem.slabodnik@gmail.com>
2012-09-30 12:58:46 +0300

    Change from email

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,8 +1,8 @@
 class ContactMailer < ActionMailer::Base
-  default from: "artem.slabodnik@gmail.com"
+  default from: "petya.petinych@gmail.com"
 
   def new_contact(contact)
   	@contact = contact
-  	mail(:to => "artem.slabodnik@gmail.com", :subject => "Contact Us")
+  	mail(:to => "petya.petinych@gmail.com", :subject => "Contact Us")
   end
 end
